ee231aaa5c66f1036cbd9a860273bd5e265409c7
NuelUzoma <emcool17@gmail.com>
2023-01-24 23:42:34 +0100

    Code Fix: Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 
 /**
  * bubble_sort - sorts an array of integers in ascending order
- * @*array: the array of integers
+ * @array: the array of integers
  * @size: the size of the array
  * Return: sorted array
  */
@@ -12,15 +12,15 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j, k;
 
-	for (i = 0; i < size-1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size-i-1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j] > array[j+1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 			for (k = 0; k < size; k++)
 			{
 				printf("%d ", array[k]);
