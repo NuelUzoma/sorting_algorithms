e6112248c085d5d6c1f3bde7fb42892942c11e46
NuelUzoma <emcool17@gmail.com>
2023-01-25 10:32:07 +0100

    Code Update: Task 2(Selection Sort)

---
+++ 2-selection_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#include <stdlib.h>
+
+/**
+ * selection_sort - sorts an array with Selection Sorting Algorithm
+ * @array: the array of unsorted integers
+ * @size: size of the array
+ * Return: the sorted array
+ */
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, jMin;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		jMin = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[jMin])
+			{
+				jMin = j;
+			}
+		}
+		if (jMin != i)
+		{
+			temp = array[i];
+			array[i] = array[jMin];
+			array[jMin] = temp;
+			for (j = 0; j < size; j++)
+			{
+				printf("%d, ", array[j]);
+			}
+			printf("\n");
+		}
+	}
+}
